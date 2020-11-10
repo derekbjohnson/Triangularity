e0ee5045fe67dfd6d22104ec6de1cec505d1b38c
derekbjohnson <dj.secure@protonmail.com>
2020-11-10 14:08:07 -0600

    Initial testing commit.  Preparing for BlazorUI

---
+++ Triangularity.Tests/ImageTests.cs
@@ -0,0 +1,53 @@
+using Xunit;
+using Triangularity.Models;
+using System.Drawing;
+
+namespace Triangularity.Tests
+{
+    public class ImageTests
+    {
+        [Theory]
+        [InlineData('b',3,10,20,10,10,20,20)]
+        [InlineData('c',10,50,20,40,20,50,30)]
+        public void GetVerticesByRowCol_ShouldBeLocated(char row, int col, int v1x, int v1y, int v2x, int v2y, int v3x, int v3y)
+        {
+            var img = new Image();
+
+            var expected = new Triangle
+            {
+                Vertex1 = new Point(v1x, v1y),
+                Vertex2 = new Point(v2x, v2y),
+                Vertex3 = new Point(v3x, v3y)
+            };
+
+            var actual = img.GetVerticesByRowCol(row, col);
+
+            Assert.Equal(expected.Vertex1, actual.Vertex1);
+            Assert.Equal(expected.Vertex2, actual.Vertex2);
+            Assert.Equal(expected.Vertex3, actual.Vertex3);
+        }
+
+        [Fact]
+        public void GetLocationByVertices_Invalid()
+        {
+            var img = new Image();
+
+            Assert.Null(img.GetLocationByVertices(-1, -1, -1, -1, -1, -1));
+        }
+
+        [Theory]
+        [InlineData("B3", 10, 20, 10, 10, 20, 20)]
+        [InlineData("C10", 50, 20, 40, 20, 50, 30)]
+        public void GetLocationByVertices_ShouldBeLocated(string expected, int v1x, int v1y, int v2x, int v2y, int v3x, int v3y)
+        {
+            var img = new Image();
+
+            string actual = img.GetLocationByVertices(v1x, v1y, v2x, v2y, v3x, v3y);
+
+            Assert.Equal(expected, actual);
+
+        }
+
+        //TODO: Test image validity by image and triangle leg size
+    }
+}
---
+++ Triangularity.Tests/TriangleTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+using Triangularity.Models;
+using System.Drawing;
+using System;
+
+namespace Triangularity.Tests
+{
+    public class TriangleTests
+    {
+        [Fact]
+        public void TriangleInvalid()
+        {
+            var testTriangle = new Triangle()
+            {
+                Vertex1 = new Point(0, 0),
+                Vertex2 = new Point(10, 0),
+                Vertex3 = new Point(0, 10)
+            };
+            Assert.Throws<ArgumentOutOfRangeException>(() => testTriangle.LegSize = 0);
+            Assert.False(testTriangle.IsValid());
+        }
+    }
+}
